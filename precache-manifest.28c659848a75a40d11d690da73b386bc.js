e6bce234bf52e0b0c9656d7d16d1d6ece9047371
d-sharmacpr <dsharmacpr@gmail.com>
2022-11-05 20:35:20 +0530

    Updates

---
+++ precache-manifest.28c659848a75a40d11d690da73b386bc.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba9beca15b45243c7cff",
+    "url": "/omcomputer/static/css/main.343897fc.chunk.css"
+  },
+  {
+    "revision": "ba9beca15b45243c7cff",
+    "url": "/omcomputer/static/js/main.b7b4fc40.chunk.js"
+  },
+  {
+    "revision": "0fec894d1ee3eb4d376c",
+    "url": "/omcomputer/static/js/runtime~main.d1a6af5f.js"
+  },
+  {
+    "revision": "78d58fad68926c025aff",
+    "url": "/omcomputer/static/css/2.226f225b.chunk.css"
+  },
+  {
+    "revision": "78d58fad68926c025aff",
+    "url": "/omcomputer/static/js/2.fb550cee.chunk.js"
+  },
+  {
+    "revision": "1d7a8b94840cd47e143a",
+    "url": "/omcomputer/static/js/3.b9da3b11.chunk.js"
+  },
+  {
+    "revision": "5b5f994d27b5e4fa84358697ad409627",
+    "url": "/omcomputer/static/media/4.5b5f994d.jpg"
+  },
+  {
+    "revision": "edd4c30d423c5d8a3d3463f798e34604",
+    "url": "/omcomputer/static/media/home_car2.edd4c30d.jpg"
+  },
+  {
+    "revision": "2e2345e5575c38cbdc9c2eb1d48976d7",
+    "url": "/omcomputer/static/media/home_car1.2e2345e5.jpg"
+  },
+  {
+    "revision": "647f9c1d4213f92fbe09d6e3d01a53a1",
+    "url": "/omcomputer/static/media/home_car3.647f9c1d.jpg"
+  },
+  {
+    "revision": "1a6685ecb80f7ed9eae82330c8fe004f",
+    "url": "/omcomputer/static/media/home_computer_ass.1a6685ec.png"
+  },
+  {
+    "revision": "8133a8eb4a3907f7f6ce29395d0cb01c",
+    "url": "/omcomputer/static/media/home_computer_rep.8133a8eb.jpg"
+  },
+  {
+    "revision": "fe1d339050bd37cafea4561c5137c51a",
+    "url": "/omcomputer/static/media/home_laptop_chip.fe1d3390.jpg"
+  },
+  {
+    "revision": "793e6ea1cd8a551ecf2455cb02738e67",
+    "url": "/omcomputer/static/media/manoj.793e6ea1.jpg"
+  },
+  {
+    "revision": "e9f07b1df71e0874c1a6a14ba6fe6b6f",
+    "url": "/omcomputer/static/media/1.e9f07b1d.jpg"
+  },
+  {
+    "revision": "2a75d4d5727bdf3cd743d66b131d6e43",
+    "url": "/omcomputer/static/media/2.2a75d4d5.jpg"
+  },
+  {
+    "revision": "7bb4d2171bd64fa0f013dcf7305f80d9",
+    "url": "/omcomputer/static/media/3.7bb4d217.jpg"
+  },
+  {
+    "revision": "995820d537bf8bc69b963ba14930ef65",
+    "url": "/omcomputer/static/media/5.995820d5.jpg"
+  },
+  {
+    "revision": "f5d66c12dd043c5118ba1c3dfdf8ebc1",
+    "url": "/omcomputer/static/media/6.f5d66c12.jpg"
+  },
+  {
+    "revision": "32a631996cd15085b634110fcfd85c75",
+    "url": "/omcomputer/static/media/head_antivirus.32a63199.jpg"
+  },
+  {
+    "revision": "8d5982b0d17cb6d7f0eb634a528934ca",
+    "url": "/omcomputer/static/media/head_computer_ass.8d5982b0.jpg"
+  },
+  {
+    "revision": "5e05fdf739b3a9a5da0e77b6cb1370dd",
+    "url": "/omcomputer/static/media/head_computer_chip.5e05fdf7.jpg"
+  },
+  {
+    "revision": "1bcc221b33270e4de5cb7b3e97aaf6db",
+    "url": "/omcomputer/static/media/head_computer_rep.1bcc221b.jpg"
+  },
+  {
+    "revision": "e929f4ae9abd7f51edf64e26ff51a461",
+    "url": "/omcomputer/static/media/head_lcd.e929f4ae.jpg"
+  },
+  {
+    "revision": "96f24949843d44610513a6313a6ad29c",
+    "url": "/omcomputer/static/media/head_mobile.96f24949.jpg"
+  },
+  {
+    "revision": "4162c4d10ce3b54d34cff82b4b8ab2cf",
+    "url": "/omcomputer/static/media/03_mouse_lenovo.4162c4d1.jpg"
+  },
+  {
+    "revision": "30690e7f9e6cd3894cf43a715de731f4",
+    "url": "/omcomputer/static/media/04_key_alfa+.30690e7f.jpg"
+  },
+  {
+    "revision": "adf286a1cb1ac5d4ecef31c8cc3b4540",
+    "url": "/omcomputer/static/media/06_key_pro.adf286a1.webp"
+  },
+  {
+    "revision": "49bcd6eb447179e4175791c1baed6ab8",
+    "url": "/omcomputer/static/media/07_anti_quick.49bcd6eb.jpg"
+  },
+  {
+    "revision": "cb2347836c8d87b29454103cd2e782fb",
+    "url": "/omcomputer/static/media/08_anti_guardian.cb234783.jpg"
+  },
+  {
+    "revision": "2449e0e376a4f924cd7f49c66908e8f8",
+    "url": "/omcomputer/static/media/09_anti_k7.2449e0e3.jpg"
+  },
+  {
+    "revision": "5c1719f10910a65780bf852493dfff55",
+    "url": "/omcomputer/static/media/10_cable_hdmi.5c1719f1.jpg"
+  },
+  {
+    "revision": "5312803718f8af00c3f41d2cae830625",
+    "url": "/omcomputer/static/media/11_cable_vga.53128037.jpg"
+  },
+  {
+    "revision": "408f9cda79cd44fc8bff2582ca0adb57",
+    "url": "/omcomputer/static/media/13_dcable_mi.408f9cda.jpg"
+  },
+  {
+    "revision": "444bf2e0a116b07c4737abc94c4f9a03",
+    "url": "/omcomputer/static/media/14_cable_printer.444bf2e0.jpg"
+  },
+  {
+    "revision": "92677329ceb884af6531f86e2ed1b030",
+    "url": "/omcomputer/static/media/16_pendrive_hp.92677329.jpg"
+  },
+  {
+    "revision": "fbfbe1c6f17055fda8321d8c409cf98c",
+    "url": "/omcomputer/static/media/17_pendrive_kig.fbfbe1c6.jpg"
+  },
+  {
+    "revision": "3550d9bf392fd243612568843541cbac",
+    "url": "/omcomputer/static/media/18_pendrive_sandisk.3550d9bf.jpg"
+  },
+  {
+    "revision": "7d8036c9980e92dd713d95d84e9d48e3",
+    "url": "/omcomputer/static/media/19_printer_car.7d8036c9.jpg"
+  },
+  {
+    "revision": "043e3a83e2d6d62c22392095855c320f",
+    "url": "/omcomputer/static/media/21_ram.043e3a83.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/omcomputer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d7c16d9738b320491e2b8c70a7722ede",
+    "url": "/omcomputer/static/media/logo.d7c16d97.png"
+  },
+  {
+    "revision": "b9a43df2901793b261bc4f5bce55c67f",
+    "url": "/omcomputer/static/media/22_laptopadop.b9a43df2.jpg"
+  },
+  {
+    "revision": "bd75f403b96c96eed3921be12133628a",
+    "url": "/omcomputer/static/media/23_powercable.bd75f403.jpeg"
+  },
+  {
+    "revision": "34d2b385aa1b2615c676b74b9844083e",
+    "url": "/omcomputer/static/media/24_epson_ink.34d2b385.jpg"
+  },
+  {
+    "revision": "094fe1a460c9297b108083657bd4359c",
+    "url": "/omcomputer/index.html"
+  }
+];
